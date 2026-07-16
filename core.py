4895c27f5d9e98d8e87056dd6cf1a2b38c665f03
Faticc <komeristyj07@gmail.com>
2026-07-16 03:43:33 +0300

    first commit

---
+++ core.py
@@ -0,0 +1,14 @@
+import grpc
+from mcproto.proto import (
+    auth_pb2_grpc,
+    client_pb2_grpc,
+    update_pb2_grpc
+)
+
+SERVER = "launchernew.mcskill.ru:443"
+
+CHANNEL = grpc.secure_channel(SERVER, grpc.ssl_channel_credentials())
+
+AUTH_STUB = auth_pb2_grpc.AuthServiceStub(CHANNEL)
+CLIENT_STUB = client_pb2_grpc.ClientServiceStub(CHANNEL)
+UPDATE_STUB = update_pb2_grpc.UpdateServiceStub(CHANNEL)
---
+++ proto.py
@@ -0,0 +1,17 @@
+import mcproto.protos.auth_pb2 as auth_pb2
+import mcproto.protos.auth_pb2_grpc as auth_pb2_grpc
+
+import mcproto.protos.client_pb2 as client_pb2
+import mcproto.protos.client_pb2_grpc as client_pb2_grpc
+
+import mcproto.protos.update_pb2 as update_pb2
+import mcproto.protos.update_pb2_grpc as update_pb2_grpc
+
+__all__ = [
+    "auth_pb2",
+    "auth_pb2_grpc",
+    "client_pb2",
+    "client_pb2_grpc",
+    "update_pb2",
+    "update_pb2_grpc",
+]
